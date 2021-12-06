16c0288974e0ba540952933a228f25d486eda4fd
Chetan <chetanoli2012@gmail.com>
2021-12-06 20:39:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BigSuccessButton, DangerButton } from "./composition";
+
+function App() {
+  return (
+    <>
+      <DangerButton text="Don't do it!" />
+      <BigSuccessButton text="Yes!!!" />
+    </>
+  );
+}
+
+export default App;
